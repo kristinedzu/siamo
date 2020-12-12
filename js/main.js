80da2e7c39cfa0a36d21bea02f58fa8e513c082d
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-12-12 16:47:06 +0100

    events layout in progress

--- js/main.js
+++ js/main.js
@@ -198,24 +198,3 @@ function appendEventsByCategory(posts) {
 //     `;
 // }
 
-// EVENTS
-fetch("http://kasialaniecka.com/siamo/wp-json/tribe/events/v1/events")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (json) {
-        appendEvent(json);
-    });
-
-function appendEvent(data) {
-    for (const event of data.events) {
-        console.log(event);
-
-        document.querySelector('.event').innerHTML += `
-            <h1>${event.title}</h1>
-            <p>${event.description}</p>
-            <p>Date: ${event.start_date_details.day}/${event.start_date_details.month}/${event.start_date_details.year} at ${event.start_date_details.hour}:${event.start_date_details.minutes}</p>
-         `;
-
-    }
-}
