0ce9f96996de2ff0e2b0186f9d78b1ffac5a78e5
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-12-12 18:32:54 +0100

    events small progress with dynamic page change

--- js/events.js
+++ js/events.js
@@ -4,18 +4,50 @@ fetch("http://kasialaniecka.com/siamo/wp-json/tribe/events/v1/events")
         return response.json();
     })
     .then(function (json) {
-        appendEvent(json);
+        appendCalendar(json);
     });
 
-function appendEvent(data) {
+function appendCalendar(data) {
     for (const event of data.events) {
-        console.log(event);
-
-        document.querySelector('.event').innerHTML += `
-            <h1>${event.title}</h1>
-            <p>${event.description}</p>
-            <p>Date: ${event.start_date_details.day}/${event.start_date_details.month}/${event.start_date_details.year} at ${event.start_date_details.hour}:${event.start_date_details.minutes}</p>
+        document.querySelector('.events-calendar').innerHTML += `
+        <div class="events-calendar-line" onclick="selectEventId(this.id)" id="${event.id}">
+        <p>${event.start_date_details.day}/${event.start_date_details.month}/${event.start_date_details.year}</p>
+        <p>${event.title}</p>
+        </div>
          `;
-
     }
+}
+
+
+async function selectEventId(something) {
+    let response = await fetch(`http://kasialaniecka.com/siamo/wp-json/tribe/events/v1/events/${something}`);
+    let data = await response.json();
+    appendEvent(data);
+}
+
+function appendEvent(event) {
+    console.log(event);
+    document.querySelector('.event-info').innerHTML += `
+        <h2>${event.title}</h2>
+        <div class="event-info-line">
+            <p>Date:</p>
+            <p>${event.start_date_details.day}/${event.start_date_details.month}/${event.start_date_details.year}</p>
+        </div>
+        <div class="event-info-line">
+            <p>Time:</p>
+            <p>${event.start_date_details.hour}/${event.start_date_details.minutes}</p>
+        </div>
+        <div class="event-info-line">
+            <p>Place:</p>
+            <p>${event.venue.venue}</p>
+        </div>
+        <div class="event-info-line">
+            <p>Address:</p>
+            <p>${event.venue.address}</p>
+        </div>
+        <div class="event-info-line">
+            <p>Price:</p>
+            <p>${event.cost}</p>
+        </div>
+    `;
 }
